83aadef1287d5d3231145f6fb2414042a0464140
Shivangi Srivastava <shi06dec@gmail.com>
2023-09-24 17:12:06 -0600

    Using Our Custom Hook - Conditionally Rendering our First Components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,20 +6,29 @@ import Header from "components/Appointment/Header";
 
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
-      {props.interview ?
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
           onDelete={() => console.log("CONFIRM")}
           onEdit={() => console.log("EDIT")}
         />
-        :
-        <Empty onAdd={() => console.log("CREATE")} />}
+      )}
+      {mode === EMPTY && <Empty onAdd={() => console.log("CREATE")} />}
     </article>
   );
 }
