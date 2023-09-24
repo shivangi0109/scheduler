d68be053171a71678216db8d179ec99c779718bb
Shivangi Srivastava <shi06dec@gmail.com>
2023-09-24 15:24:05 -0600

    Adding Transition - Added a second test to help us develop the transition functionality & a transition function within useVisualMode, this function will take in a new mode and update the mode state with the new value

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,7 +3,15 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
-  return { mode };
+  // Create the 'transition' function to update the 'mode' state
+  const transition = (newMode) => {
+    setMode(newMode);
+  };
+
+  return {
+    mode,
+    transition
+  };
 }
 
 // As seen here, the `useVisualMode` function can take an initial argument to set the mode state. We then return an object `{ mode }`, which can also be written as `{ mode: mode }`. This lets our tests (and components) access the current value of the mode from the hook.
