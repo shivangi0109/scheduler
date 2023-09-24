1d34aab308b7e16fc9d6d0f44e580bc4637ce82f
Shivangi Srivastava <shi06dec@gmail.com>
2023-09-24 17:29:08 -0600

    Using Our Custom Hook - The Form Component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,6 +6,7 @@ import Header from "components/Appointment/Header";
 
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Form from "components/Appointment/Form";
 import useVisualMode from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
@@ -30,6 +31,9 @@ export default function Appointment(props) {
         />
       )}
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && (
+        <Form interviewers={props.interviewers} />
+      )}
     </article>
   );
 }
