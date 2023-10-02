9c98c634d2973e85e81d914a0e98ca6f2b086961
Shivangi Srivastava <shi06dec@gmail.com>
2023-10-02 14:37:05 -0600

    Integration testing - Debugging tests

--- src/hooks/__tests__/Application.test.js
+++ src/hooks/__tests__/Application.test.js
@@ -2,7 +2,7 @@
   We are rendering `<Application />` down below, so we need React.createElement
 */
 import React from "react";
-import { waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
+import { waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
 
 /*
   We import our helper functions from the react-testing-library
@@ -41,7 +41,7 @@ describe("Application", () => {
   });
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
@@ -62,5 +62,23 @@ describe("Application", () => {
     fireEvent.click(getByText(appointment, "Save"));
 
     // console.log(prettyDOM(appointment));
+
+    // Use debug to output the current state of the DOM
+    // debug();
+
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+    // debug();
+
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+    // console.log(prettyDOM(day));
+
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+
+    // debug();
   });
 });
