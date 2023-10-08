b5be501e19524b2d634a264de9b5f5fc3b5a9348
Shivangi Srivastava <shi06dec@gmail.com>
2023-10-08 15:23:51 -0600

    Add build files

---
+++ build/precache-manifest.c6cc860a4ce0b3fd3aaabb965d2b2dcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2de134c8877f9c5fb978f03cad65b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84497595fa369e1fc67c",
+    "url": "/static/css/2.17e61147.chunk.css"
+  },
+  {
+    "revision": "1285b52b12ea045872a7",
+    "url": "/static/css/main.15c4832a.chunk.css"
+  },
+  {
+    "revision": "84497595fa369e1fc67c",
+    "url": "/static/js/2.f27e0975.chunk.js"
+  },
+  {
+    "revision": "c91cbb924d2e6dd2f7d23f894d2489e8",
+    "url": "/static/js/2.f27e0975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1285b52b12ea045872a7",
+    "url": "/static/js/main.48f29fd6.chunk.js"
+  },
+  {
+    "revision": "243b8f0ab90e65c7feeb",
+    "url": "/static/js/runtime-main.89e9d1db.js"
+  }
+]);
